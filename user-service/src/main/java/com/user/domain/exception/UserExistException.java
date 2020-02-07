3492a6f035b5248f1b4344e0072591b920f32d0c
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-02-07 21:56:16 +0100

    User-service - update exceptions

--- user-service/src/main/java/com/user/domain/exception/UserExistException.java
+++ user-service/src/main/java/com/user/domain/exception/UserExistException.java
@@ -1,13 +1,9 @@
 package com.user.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Author Kamil Seweryn
  */
 
-/*@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "This user exist in the database!")*/
 public class UserExistException extends Exception {
     public UserExistException(String email) {
         super("User with e-mail: " + email + " already exist in the database!");
--- user-service/src/main/java/com/user/domain/exception/UserNotExistException.java
+++ user-service/src/main/java/com/user/domain/exception/UserNotExistException.java
@@ -1,15 +1,9 @@
 package com.user.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /**
  * Author Kamil Seweryn
  */
 
-/*
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-*/
 public class UserNotExistException extends Exception {
     public UserNotExistException() {
         super("This dosen't exist in the database!");
