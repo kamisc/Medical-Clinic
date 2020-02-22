3ce1ec405442b20ce29a082b52847f3cb5261b21
Kamil Seweryn <kamil_seweryn@o2.pl>
2020-02-22 12:48:27 +0100

    Add VisitRepository

---
+++ visit-service/src/main/java/com/visit/repository/VisitRepository.java
@@ -0,0 +1,30 @@
+package com.visit.repository;
+
+import com.visit.domain.Visit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Author Kamil Seweryn
+ */
+
+@Repository
+@Transactional
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+    List<Visit> findAllByUserId(Long userId);
+
+    List<Visit> findAllByDoctorId(Long doctorId);
+
+    @Override
+    Optional<Visit> findById(Long id);
+
+    @Override
+    Visit save(Visit visit);
+
+    @Override
+    void delete(Visit visit);
+}
